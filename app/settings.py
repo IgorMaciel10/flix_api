3e27480a649eb57b27fca91c1166f3c3309e3adc
Igor Santos Maciel <igorsantosmaciell@gmail.com>
2026-01-04 22:56:41 -0300

    novo installed app e tempo de expiração do token JWT

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,10 +39,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework_simplejwt",
     "genres",
     "actors",
     "movies",
-    "reviews"
+    "reviews",
+    "authentication",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +128,17 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+    
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+    
+}
+
+SIMPLE_JWT = {
+      "ACESS_TOKEN_LIFETIME": timedelta(),
+      "REFRESH_TOKEN_LIFETIME": timedelta()
+}
